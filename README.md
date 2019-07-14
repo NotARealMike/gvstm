73880e90bbe8a9d21b6f0f7fe4a8e96238e28a47
Mike Cachopo <rui@cachopo.org>
2019-07-14 09:17:32 +0100

    Initital basic implementation.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gvstm
-A virtual software transactional memory implementation in Go.
+A software transactional memory implementation in Go.
+
+Current version is only a demo. This is a project I hope to have the chance to develop and extend in the coming months.
+
+### Implementation notes
+
+Use of versioned boxes based on [an exisitng Java implementation.](https://github.com/inesc-id-esw/jvstm)
+
+### License
+
+This project is currently not covered by any open source license. I intend to add one after I have a stable release.
---
+++ stm.go
@@ -0,0 +1,139 @@
+package gvstm
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+var (
+	globalClock clock
+	commitMutex sync.Mutex
+)
+
+type clock uint64
+
+func (c *clock) updateClock(t uint64) {
+	atomic.StoreUint64((*uint64)(c), t)
+}
+
+func (c *clock) sampleClock() (t uint64) {
+	return atomic.LoadUint64((*uint64)(c))
+}
+
+type VBox struct {
+	body atomic.Value
+}
+
+func NewVBox(initial interface{}) *VBox {
+	body := vBody{initial, 0, nil}
+	vb := &VBox{}
+	vb.body.Store(body)
+	return vb
+}
+
+func (vb *VBox) read(seqNo uint64) interface{} {
+	body := vb.body.Load().(vBody)
+	for body.seqNo > seqNo {
+		body = *body.prev
+	}
+	return body.value
+}
+
+func (vb *VBox) commit(seqNo uint64, value interface{}) {
+	prev := vb.body.Load().(vBody)
+	body := vBody{value, seqNo, &prev}
+	vb.body.Store(body)
+}
+
+type vBody struct {
+	value interface{}
+	seqNo uint64
+	prev *vBody
+}
+
+type Transaction interface {
+	commit() bool
+	Read(vb *VBox) interface{}
+	Write(vb *VBox, value interface{})
+}
+
+type rWTransaction struct {
+	readVersion uint64
+	readSet map[*VBox]struct{}
+	writeSet map[*VBox]interface{}
+}
+
+func newRWTransaction() *rWTransaction {
+	return &rWTransaction{
+		globalClock.sampleClock(),
+		map[*VBox]struct{}{},
+		map[*VBox]interface{}{},
+	}
+}
+
+func (tx *rWTransaction) commit() bool {
+	if len(tx.writeSet) == 0 {
+		return true
+	}
+	commitMutex.Lock()
+	for vb := range tx.readSet {
+		if vb.body.Load().(vBody).seqNo > tx.readVersion {
+			commitMutex.Unlock()
+			return false
+		}
+	}
+	writeVersion := globalClock.sampleClock() + 1
+	for vb, value := range tx.writeSet {
+		vb.commit(writeVersion, value)
+	}
+	globalClock.updateClock(writeVersion)
+	commitMutex.Unlock()
+	return true
+}
+
+func (tx *rWTransaction) Read(vb *VBox) interface{} {
+	if value, ok := tx.writeSet[vb]; ok {
+		return value
+	}
+	tx.readSet[vb] = struct {}{}
+	return vb.read(tx.readVersion)
+}
+
+func (tx *rWTransaction) Write(vb *VBox, value interface{}) {
+	tx.writeSet[vb] = value
+}
+
+type readOnlyTransaction struct {
+	readVersion uint64
+}
+
+func newReadOnlyTransaction() *readOnlyTransaction {
+	return &readOnlyTransaction{globalClock.sampleClock()}
+}
+
+func (tx *readOnlyTransaction) commit() bool {
+	return true
+}
+
+func (tx *readOnlyTransaction) Read(vb *VBox) interface{} {
+	return vb.read(tx.readVersion)
+}
+
+func (tx *readOnlyTransaction) Write(vb *VBox, value interface{}) {
+	panic("write in a read only transaction")
+}
+
+func Atomic(f func(tx Transaction) interface{}, ro bool) interface{} {
+RETRY:
+	var tx Transaction
+	if ro {
+		tx = newReadOnlyTransaction()
+	} else {
+		tx = newRWTransaction()
+	}
+	value := f(tx)
+	if !tx.commit() {
+		goto RETRY
+	}
+	return value
+}
