cf41266c773b7386b83c00545bf4ba77b3ab60bd
Mike Cachopo <mike@cachopo.org>
2020-04-14 17:09:42 +0100

    Add ImmutableConnectionImpl.

---
+++ stmbench7/interfaces/immutableCollectionImpl.go
@@ -0,0 +1,34 @@
+package interfaces
+
+type ImmutableCollectionImpl struct {
+    elements []interface{}
+}
+
+func newImmutableCollectionImpl(elements []interface{}) *ImmutableCollectionImpl {
+    ic := &ImmutableCollectionImpl{make([]interface{}, len(elements))}
+    for i := range elements {
+        ic.elements[i] = elements[i]
+    }
+    return ic
+}
+
+func (ic *ImmutableCollectionImpl) Size() int {
+    return len(ic.elements)
+}
+
+func (ic *ImmutableCollectionImpl) Contains(element interface{}) bool {
+    for _, e := range ic.elements {
+        if e == element {
+            return true
+        }
+    }
+    return false
+}
+
+func (ic *ImmutableCollectionImpl) Clone() ImmutableCollection {
+    return newImmutableCollectionImpl(ic.elements)
+}
+
+func (ic *ImmutableCollectionImpl) ToSlice() []interface{} {
+    return ic.elements
+}
