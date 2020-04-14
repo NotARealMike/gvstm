2a7d87611505e0def8d37bc337379a0931f197b5
Mike Cachopo <mike@cachopo.org>
2020-04-14 17:10:29 +0100

    Add ConnectionImpl.

---
+++ stmbench7/interfaces/connectionImpl.go
@@ -0,0 +1,41 @@
+package interfaces
+
+type connectionImpl struct {
+    typ string
+    length int
+    from, to AtomicPart
+}
+
+func newConnectionImpl(from, to AtomicPart, typ string, length int) Connection  {
+    return &connectionImpl{
+        typ:    typ,
+        length: length,
+        from:   from,
+        to:     to,
+    }
+}
+
+func (c *connectionImpl) GetReversed() Connection {
+    return &connectionImpl{
+        typ:    c.typ,
+        length: c.length,
+        from:   c.to,
+        to:     c.from,
+    }
+}
+
+func (c *connectionImpl) GetDestination() AtomicPart {
+    return c.to
+}
+
+func (c *connectionImpl) GetSource() AtomicPart {
+    return c.from
+}
+
+func (c *connectionImpl) GetType() string {
+    return c.typ
+}
+
+func (c *connectionImpl) GetLength() int {
+    return c.length
+}
